991af43222da9d93ed30b3151c508168c6aaa123
lu69159 <2909165527@qq.com>
2026-04-18 23:27:09 +0800

    update 0.22.9

--- scripts/planets/NTSatelite.js
+++ scripts/planets/NTSatelite.js
@@ -1,19 +1,23 @@
 const NT = require("planets/Nepture").NT;
 
-const NTsatellite = new Planet("冰卫一", NT, 0.4, 1);
-NTsatellite.localizedName = "冰卫一";
+const NTsatellite = new Planet("冰卫一", NT, 1, 1.5);
 NTsatellite.meshLoader = prov(() => new MultiMesh(
 	new HexMesh(NTsatellite, 2),
 	new HexSkyMesh(NTsatellite, 3, 0.15, 0.02, 2, Color.valueOf("C0ECFF"), 2, 0.42, 1, 0.43),
 ));
 NTsatellite.generator = extend(TantrosPlanetGenerator, {
     getColor(position ,out){
-        out.set(Color.valueOf("C0ECFF"));
+        out.set(Color.valueOf("3C3C3C"));
     }
 });
-NTsatellite.accessible = false;
-NTsatellite.bloom = false;
-NTsatellite.hasAtmosphere = false;
-NTsatellite.orbitRadius = 10;
-NTsatellite.orbitTime = 60 * 60;
+Object.assign(NTsatellite, {
+    localizedName: "实验卫星",
+    description: "各种实验地图试验玩法",
+    accessible: false,
+    bloom: false,
+    hasAtmosphere: false,
+    orbitRadius: 10,
+    orbitTime: 60 * 60,
+});
 exports.NTsatellite = NTsatellite;
+//statParent = NT
