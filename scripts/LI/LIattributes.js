31421711bf1978d58be55d4435e2c74a0299df23
lu69159 <2909165527@qq.com>
2026-05-21 02:35:55 +0800

    删除旧内容

--- scripts/LI/LIattributes.js
+++
@@ -1,9 +0,0 @@
-function newAttr(name) {
-    exports[name] =(() => {
-        const c = Attribute.add(name);
-        return c;
-    })();
-}
-
-newAttr("cryofluid");
-newAttr("scrapfluid");
--- scripts/LI/LIweathers.js
+++
@@ -1,4 +0,0 @@
-const JH = new ParticleWeather("极寒");
-exports.极寒 = JH;
-const BX = new ParticleWeather("暴雪");
-exports.暴雪 = BX;
