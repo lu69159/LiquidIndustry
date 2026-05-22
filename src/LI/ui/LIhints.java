ea28033457dc7933507e8b9979dcfe7b8c843beb
lu69159 <2909165527@qq.com>
2026-05-22 13:14:05 +0800

    TODO

---
+++ src/LI/ui/LIhints.java
@@ -0,0 +1,78 @@
+package LI.ui;
+
+
+import arc.Core;
+//import arc.Events;
+import arc.func.*;
+import arc.util.*;
+
+import static mindustry.Vars.*;
+//import static mindustry.game.EventType.*;
+import static mindustry.ui.fragments.HintsFragment.*;
+
+//TODO
+public class LIhints{
+    public LIhints(){
+        Core.app.post(() -> {
+            ui.hints.hints.add(LIHint.values());
+        });
+    }
+
+    public enum LIHint implements Hint {
+        ; //TODO: enum hint
+
+        @Nullable
+        String text;
+        int visibility = visibleAll;
+        Hint[] dependencies = {};
+        boolean finished, cached;
+        Boolp complete, shown = () -> true;
+
+        LIHint(Boolp complete){
+            this.complete = complete;
+        }
+
+        @Override
+        public boolean finished(){
+            if(!cached){
+                cached = true;
+                finished = Core.settings.getBool(name() + "-hint-done", false);
+            }
+            return finished;
+        }
+
+        @Override
+        public void finish(){
+            Core.settings.put(name() + "-hint-done", finished = true);
+        }
+
+        @Override
+        public String text(){
+            if(text == null){
+                text = mobile && Core.bundle.has("hint." + name() + ".mobile") ? Core.bundle.get("hint." + name() + ".mobile") : Core.bundle.get("hint." + name());
+                if(!mobile) text = text.replace("tap", "click").replace("Tap", "Click");
+            }
+            return text;
+        }
+
+        @Override
+        public boolean complete(){
+            return complete.get();
+        }
+
+        @Override
+        public boolean show(){
+            return shown.get() && (dependencies.length == 0 || !Structs.contains(dependencies, d -> !d.finished()));
+        }
+
+        @Override
+        public int order(){
+            return ordinal();
+        }
+
+        @Override
+        public boolean valid(){
+            return (mobile && (visibility & visibleMobile) != 0) || (!mobile && (visibility & visibleDesktop) != 0);
+        }
+    }
+}
